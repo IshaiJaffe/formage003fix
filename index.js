86df261d01d7096e17db6f0a48245657d21a5fd1
Ishai <ishai@empeeric.com>
2013-05-13 15:17:02 +0300

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+    Formage
+    based on forms
+ */
+
+module.exports.forms = require('./forms');
+
+module.exports.fields = require('./fields');
+
+module.exports.widgets = require('./widgets');
+
+module.exports.types = require('./mongoose-types');
+
+module.exports.loadTypes = module.exports.types.loadTypes;
+
+module.exports.setAmazonCredentials = module.exports.fields.setAmazonCredentials;
+
+module.exports.register_models = module.exports.forms.set_models;
+
+module.exports.serve_static = function(app,express) {
+    app.use(require('express').static(require('path').join(__dirname,'public')));
+};
